6e58179eca9378a953036a9412fbf6cac73aab23
Jonathan Wei <jonathan.wei@batechworks.com>
2023-04-21 15:54:49 +0800

    add hello world qr

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use qrcode::QrCode;
+
 fn main() {
-    println!("Hello, world!");
+    let code = QrCode::new("Hello, world!").unwrap();
+    let qr_string = code
+        .render::<char>()
+        .quiet_zone(false)
+        .module_dimensions(2, 1)
+        .build();
+    println!("{}", qr_string);
 }
