b81793b24608e07949deee6aff2c2c83ae682f98
Weidaicheng <wdcdavyc@outlook.com>
2023-04-27 19:46:25 +0800

    add hello example

---
+++ examples/hello/src/main.rs
@@ -0,0 +1,34 @@
+use std::io::{stdout, BufWriter};
+
+use ferris_says::say;
+use qr_code_cli::{command::Command, flag::FlagValue};
+
+fn main() {
+    let mut command = Command::new("gives a friendly hello", "hello TEXT", |text, flags| {
+        let mut use_ferris = false;
+
+        for flag in flags {
+            if flag.is_match("-f") {
+                match flag.value {
+                    FlagValue::Bool(value) => {
+                        use_ferris = value;
+                    }
+                    _ => (),
+                }
+            }
+        }
+
+        let message = format!("hello, {}!", text.unwrap());
+        if use_ferris {
+            let stdout = stdout();
+            let width = message.chars().count();
+
+            let mut writer = BufWriter::new(stdout.lock());
+            say(message.as_str(), width, &mut writer).unwrap();
+        } else {
+            println!("{}", message);
+        }
+    });
+    command.add_boolean_flag("f", "ferris", "say hello from ferris");
+    command.execute();
+}
